915c266fd78d41a1664fb26599a984085662fccb
Fuad Soudah <Fuadex@gmail.com>
2017-05-16 23:40:42 +0200

    Update 0.8
    Alpha Plus

---
+++ footer.js
@@ -0,0 +1,25 @@
+document.write('\
+  \
+<div class="container-fluid">\
+    <div class="row">\
+<nav class="navbar navbar-inverse navbar-bottom" role="navigation">\
+  <div class="navbar-header">\
+    <a class="navbar-brand" href="#">Future Projects:</a>\
+  </div>\
+    <ul class="nav navbar-nav">\
+  <li><a href="#">Tertile;s Uproar</a></li>\
+  <li><a href="#">Sehnsucht</a></li>\
+  <li><a href="#">Introspection</a></li>\
+  <li><a href="#">Dorfbladt</a></li>\
+  <li><a href="#">Grassausschus</a></li>\
+  <li><a href="#">Untitled Video</a></li>\
+    </ul>\
+\
+        <ul class="nav navbar-nav navbar-right list-inline">\
+      <li><a href="Contact.html"><span class="glyphicon glyphicon-zoom-in"></span> Retrospection</a></li>\
+    </ul>\
+\
+</nav>\
+</div>\
+</div>\
+');
